47e92f9d9b591b858fdaa0a401ae8afc5bab4a42
Debarshi Raj Basumatary <basumatary18@gmail.com>
2026-09-01 11:39:11 +0530

    chapter 2 initial comment

---
+++ Chapter-2-Programming-Guess-Game/src/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    println!("Guess The Number Game!");
+    println!("Enter a number:");
+
+    // let a = 5; Default, variable is immutable.
+    // mut makes it mutable
+    let mut guess = String::new(); 
+
+}
